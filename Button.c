f44d69c4bad1902f7e55d868af57b1d4b7a0db26
boyu850609 <1103104225@gm.kuas.edu.tw>
2016-11-20 19:56:41 +0800

    second commit

--- Button.c
+++ Button.c
@@ -1,64 +1,73 @@
 #include "lpc11xx.h"
 #include "timer16.h"
 #include "clkconfig.h"
+#include "led.h"
 
-#define LED1_ON  LPC_GPIO1->DATA &= ~(1<<8)
-#define LED1_OFF LPC_GPIO1->DATA |= (1<<8)
-#define LED1_Change LPC_GPIO1->DATA^=(1<<8); // XOR
+#define LED_pin 8 //set LED_pin
+#define Key_pin 6 //set Key_pin
+#define LED_on_delay 250 // set LED_on delay time
+#define LED_off_delay 250 // set LED_off delay time
+#define LED_ON  LPC_GPIO1->DATA &=~(1<<LED_pin); //open(set) LED
+#define LED_OFF LPC_GPIO1->DATA |=(1<<LED_pin);  //close(clear) LED
+#define LED_Change LPC_GPIO1->DATA^=(1<<LED_pin); // XOR
+#define Key_press (LPC_GPIO2->DATA&(1<<Key_pin))!=(1<<Key_pin) // press key1(pin2.6)
+#define Key_release (LPC_GPIO2->DATA&(1<<Key_pin))==(1<<Key_pin) // release key1(pin2.6)
+
 
 extern volatile uint32_t timer16_0_counter;
 uint32_t msTicks=0;
 
-void SysTick_Handler(void)
+/* system Clock */
+void SysTick_Handler(void) 
 {
-   msTicks++;
+   msTicks++;// plus System Clock
 }
 
-void led_init()
-{
-   LPC_SYSCON->SYSAHBCLKCTRL |= (1<<16); // 
-   LPC_IOCON->PIO0_1 &= ~0x07;
-   LPC_IOCON->PIO0_1 |= 0x01; //
-   LPC_SYSCON->SYSAHBCLKCTRL &= ~(1<<16); // 
-   LPC_GPIO1->DIR |= (1<<8); // 
-   LPC_GPIO1->DATA |= (1<<8); // 
-}
-int main()
-{
-   led_init();
-   T16B0_init();
-	 while(1){
-		 if((LPC_GPIO2->DATA&(1<<6))!=(1<<6)){
-					T16B0_delay_ms(2);
-					msTicks=0;
+void Button_Handler(void){
+	while(1){
+		 /* press key1(button)_pin2.6 */
+		 if(Key_press){ 
+				T16B0_delay_ms(2); // avoid for switch bounce
+				msTicks=0; // clear msTicks to zero
 				while(1){
-						SysTick_Config(SystemAHBFrequency/2500);
-						T16B0_delay_ms(2);
-						if((LPC_GPIO2->DATA&(1<<6))!=(1<<6)&&(msTicks>1000)){
-							LED1_OFF;
-									while(1){
-										LED1_ON;
-										T16B0_delay_ms(250);
-										LED1_OFF;
-										T16B0_delay_ms(250);
-										
-										if((LPC_GPIO2->DATA&(1<<6))==(1<<6)){
-										LED1_OFF;
-										msTicks=0;
-										break;
-										}
-									}
-									break;
-						}
-						
-						else if(msTicks<1000&&(LPC_GPIO2->DATA&(1<<6))==(1<<6)){
-									T16B0_delay_ms(2);
-									LED1_Change;
-							    msTicks=0;
-									break;
-						}
+					SysTick_Config(SystemFrequency/1000); // set every millisecond into interrupt
+					T16B0_delay_ms(2); // avoid for switch bounce
 					
+					/* press key1(button)_pin2.6 and press over one second */
+					if(Key_press&&(msTicks>1000)){ // plus 1000 times to count 1sec
+						LED_OFF; // clear LED
+
+						/* Enter Flashing loop */
+						while(1){
+							LED_Change;
+							T16B0_delay_ms(LED_on_delay); // delay open 0.25sec
+							
+										
+							/* release key1(button) */
+							if(Key_release){
+							LED_OFF; //	close LED
+							msTicks=0; // clear msTicks
+							break; // break Flashing loop
+							}
+						}
+						break;
+					}
+						
+					/* Press to release the button not over than one second */
+					else if(msTicks<1000&&Key_release){
+						T16B0_delay_ms(2);// avoid for switch bounce
+						LED_Change; // xor LED status
+						msTicks=0;// clear msTicks
+						break;
 					}
 			}
-		 }
+		}
+	}
+}
+
+int main()
+{
+   led_init(); // use led
+   T16B0_init(); // use timer16
+	 Button_Handler(); // use Button_Handler
 }	 
