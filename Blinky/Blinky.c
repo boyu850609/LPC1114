958cc365b1fde3ed442dae274d6c99480ea33537
boyu850609 <1103104225@gm.kuas.edu.tw>
2017-01-19 18:16:01 +0800

    third+.h.c

--- Blinky/Blinky.c
+++ Blinky/Blinky.c
@@ -3,10 +3,17 @@
 #include "timer16.h"
 
 #define LED_pin 8 //set LED_pin
-#define LED_on  LPC_GPIO1->DATA &=~(1<<LED_pin); //open(set) LED
-#define LED_off LPC_GPIO1->DATA |=(1<<LED_pin);  //close(clear) LED
 #define LED_on_delay 1000 // set LED_on delay time
 #define LED_off_delay 250 // set LED_off delay time
+
+
+static __inline void LED_on (void){
+		LPC_GPIO1->DATA &=~(1<<LED_pin); //open(set) LED
+}
+static __inline void LED_off(void){
+		LPC_GPIO1->DATA |=(1<<LED_pin); 	//close(clear) LED
+}
+
 
 int main()
 {
@@ -15,8 +22,8 @@ int main()
    while(1)
    {
       T16B0_delay_ms(LED_on_delay);  // delay LED_on 1sec
-      LED_on;
+      LED_on();
       T16B0_delay_ms(LED_off_delay); // delay LED_off 0.25sec
-      LED_off;
+      LED_off();
    }
 }
---
+++ Blinky/led.h
@@ -0,0 +1,4 @@
+#ifndef _LED_H
+void led_init(void);
+void led_delay(void);
+#endif
