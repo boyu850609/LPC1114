6bcbeef3c9eeee57916fb3e694a92ea106dae182
boyu850609 <1103104225@gm.kuas.edu.tw>
2017-01-19 21:27:36 +0800

    update

--- Flash/Flash.c
+++ Flash/Flash.c
@@ -2,19 +2,22 @@
 #include "led.h"
 
 #define LED_pin 8 //set LED_pin
-static __inline void LED_on (void){
+static __inline void LED_on (void)
+{
 		LPC_GPIO1->DATA &=~(1<<LED_pin);	//open(set) LED
 }	
-static __inline void LED_off(void){ 
+
+static __inline void LED_off(void)
+{ 
 		LPC_GPIO1->DATA |=(1<<LED_pin);	//close(clear) LED
-}  
+} 
 
-
-int main(void){
-		
+int main(void)
+{
 	led_init();  //use led
 	
-	while(1){
+	while(1)
+	{
 		LED_on();			 // open LED
 		led_delay(); // delay open 0.5sec
 		LED_off();		 //	close LED
