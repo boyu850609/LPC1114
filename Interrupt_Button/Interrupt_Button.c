6d2f1b6156061cd52ee9e209c6ddc7c34fcbf50a
boyu850609 <1103104225@gm.kuas.edu.tw>
2017-01-19 20:33:04 +0800

    update

--- Interrupt_Button/Interrupt_Button.c
+++ Interrupt_Button/Interrupt_Button.c
@@ -6,54 +6,71 @@
 #define Key_delay_timer 1000000 // Button over time into flashing LED
 #define delay_timer 600000 // led flashing 0.25sec
 
-static __inline void LED_OFF (void){ 
+static __inline void LED_OFF (void)
+{ 
 		LPC_GPIO1->DATA |=(1<<8); //close(clear) LED
 } 
 
-static __inline void LED_Change (void){ 
+static __inline void LED_Change (void)
+{ 
 		LPC_GPIO1->DATA^=(1<<8); // XOR
 }
 
-static __inline int Key_press_interrupt (void){
+static __inline int Key_press_interrupt (void)
+{
 		return (LPC_GPIO2->MIS&(1<<6))==(1<<6) ; // press key1 for interrupt(pin2.6)
 }
 
-static __inline int Key_press (void){
+static __inline int Key_press (void)
+{
 		return (LPC_GPIO2->DATA&(1<<6))!=(1<<6); // press key1(pin2.6)
 } 
 
-static __inline int Key_release (void){
+static __inline int Key_release (void)
+{
 		return (LPC_GPIO2->DATA&(1<<6))==(1<<6); // release key1(pin2.6)
 
 } 
 
-void Interrupt_init(void){
+void Interrupt_init(void)
+{
 		LPC_GPIO2->IE |= (1<<Key_pin); // set pin2.6 to generate an interrupt
 }
 
-int delay(int time){
+int delay(int time)
+{
 		long int i;
 		for(i=time;i>0;i--){};
 		return time;
 }
 
-void PIOINT2_IRQHandler(void){	/* interrupt Button function */
+/* interrupt Button function */
+void PIOINT2_IRQHandler(void)	
+{	
 		long i;
-		if(Key_press_interrupt()){
-				while( Key_press() ){
-						for(i=Key_delay_timer;i>0;i--){
+		if(Key_press_interrupt())
+		{
+				while(Key_press())
+				{
+						for(i=Key_delay_timer;i>0;i--)
+						{
 								if( Key_release())
-								break;
+										break;
 						}
-						if(Key_release()){
+						
+						if(Key_release())
+						{
 								LED_Change();
 								LPC_GPIO2->IC = 0XFFFF;
 								break;
 						}			
-						while(1){
+						
+						while(1)
+						{
 								LED_Change();
 								delay(delay_timer);
-										if(Key_release()){
+										if(Key_release())
+										{
 												LED_OFF(); //	close LED
 												LPC_GPIO2->IC = 0XFFFF;
 												break; // break Flashing loop
@@ -63,16 +80,11 @@ void PIOINT2_IRQHandler(void){	/* interrupt Button function */
 		}	
 }
 
-
-/***************************************/
-/**************** main *****************/
-/***************************************/
-
 int main()
 {
    led_init(); // use led
    T16B0_init(); // use timer16
 	 Interrupt_init();
 	 NVIC_EnableIRQ(EINT2_IRQn);// use Button_IRQHandler
-	 while(1);
+	 while(1); // loop
 }	  
