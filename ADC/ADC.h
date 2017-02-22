4741171fa931b5244f2599ea7c9557d1dbb6cdb5
boyu850609 <1103104225@gm.kuas.edu.tw>
2017-02-22 23:34:42 +0800

    first

---
+++ ADC/ADC.h
@@ -0,0 +1,6 @@
+#include "lpc11xx.h"
+
+#ifndef _LED_H
+void ADC_Init(int Channel);
+float ADC_Read(uint8_t Channel);
+#endif
---
+++ ADC/PWM.h
@@ -0,0 +1,6 @@
+#include "lpc11xx.h"
+
+#ifndef _LED_H
+void T16B0M0_PWM_Init(uint16_t cycle,float duty);
+void T16B0M0_PWM_SET(uint16_t cycle,uint8_t duty);
+#endif
